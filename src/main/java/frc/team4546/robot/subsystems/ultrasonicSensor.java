a339e5f21cc76f481c10194b58c1602c5d2fb9be
Sage Coff <pixelmoongaming4702@gmail.com>
2019-02-02 12:00:43 -0500

    quality of life changes

--- src/main/java/frc/team4546/robot/subsystems/ultrasonicSensor.java
+++ src/main/java/frc/team4546/robot/subsystems/ultrasonicSensor.java
@@ -9,17 +9,41 @@ public class ultrasonicSensor {
     private double volts;
     private int averageRaw;
     private double averageVolts;
+    private final int voltageInputWindowLength;
+    private double[] voltageInputWindow;
 
     public ultrasonicSensor(){
         sUltrasonic1 = new AnalogInput(sUltrasonic1Port);
+        voltageInputWindowLength = 5;
+        voltageInputWindow = new double[voltageInputWindowLength];
     }
+    
+    public ultrasonicSensor(final int averageWindowSize){
+        sUltrasonic1 = new AnalogInput(sUltrasonic1Port);
+        voltageInputWindowLength = averageWindowSize;
+        voltageInputWindow = new double[voltageInputWindowLength];
+    }
+
     public double getRange(){
 		AnalogInput.setGlobalSampleRate(62500);
 		raw = sUltrasonic1.getValue();
         volts = sUltrasonic1.getVoltage();
         averageRaw = sUltrasonic1.getAverageValue();
         averageVolts = sUltrasonic1.getAverageVoltage();
-		analogValue = averageRaw * 5;
-        return analogValue;
+        analogValue = averageRaw * 5;
+
+        // shift average window
+        for(int i = 0; i < (voltageInputWindowLength - 1); i++){
+            voltageInputWindow[i] = voltageInputWindow[i+1];
+        }
+        voltageInputWindow[voltageInputWindowLength - 1] = analogValue;
+        
+        // calculating average of window
+        int sum = 0;
+        for(int i = 0; i < voltageInputWindowLength; i++){
+            sum += voltageInputWindow[i];
+        }
+
+        return sum / voltageInputWindowLength;
     }
 }
