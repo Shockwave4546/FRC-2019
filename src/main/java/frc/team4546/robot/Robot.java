cfcc497cabb62119eabf5c8abc4460032c24f77b
harrisond3 <doukasharrison@gmail.com>
2019-02-02 14:23:41 -0500

    attempt at color sensor
    still don't work but it's what we got rn

--- src/main/java/frc/team4546/robot/Robot.java
+++ src/main/java/frc/team4546/robot/Robot.java
@@ -1,45 +1,23 @@
 package frc.team4546.robot;
 
-import frc.team4546.robot.subsystems.vision.Cameras;
 import frc.team4546.robot.subsystems.ColorSensor;
 
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.command.Command;
+
 import edu.wpi.first.wpilibj.command.Scheduler;
-import edu.wpi.first.wpilibj.livewindow.LiveWindow;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj.AnalogInput;
-import edu.wpi.first.wpilibj.PowerDistributionPanel;
-import edu.wpi.first.wpilibj.Talon;
+
 import edu.wpi.first.wpilibj.DriverStation;
+
+import edu.wpi.first.wpilibj.I2C;
 
 public class Robot extends TimedRobot {
 
   private int driverStationNumber = 0;
 
-  private PowerDistributionPanel m_PDP;
-
-  private static final int kMotor1Port = 13;
-  private Talon kMotor1;
-  private double kMotor1Pos = .5;
-  private double kMotor1Neg = -.5;
-
-  private static final int kMotor2Port = 14;
-  private Talon kMotor2;
-  private double kMotor2Pos = .5;
-  private double kMotor2Neg = .5;
-
-  private static final int sUltrasonic1Port = 0;
-  private AnalogInput sUltrasonic1;
-  private double analogValue;
-  private int bits;
-  private int raw;
-  private double volts;
-  private int averageRaw;
-  private double averageVolts;
+  public ColorSensor colorsensor;
 
   public XboxController xbox;
 
@@ -48,33 +26,15 @@ public class Robot extends TimedRobot {
   @Override
   public void robotInit() {
     driverStationNumber = DriverStation.getInstance().getLocation();
-    Cameras.setup(); // Setup and Connection to Pixy2 and Microsoft Camera
+    // Cameras.setup(); // Setup and Connection to Pixy2 and Microsoft Camera
 
-    SmartDashboard.putBoolean("Pixy2 Light", false); // Addition of Pixy2 Lamp Toggle
-    boolean PixyLightState = SmartDashboard.getBoolean("Pixy2 Light", false);
-    Cameras.light(PixyLightState); // Sends Current state of Toggle Button to Pixy2
+    // SmartDashboard.putBoolean("Pixy2 Light", false); // Addition of Pixy2 Lamp
+    // Toggle
+    // boolean PixyLightState = SmartDashboard.getBoolean("Pixy2 Light", false);
+    // Cameras.light(PixyLightState); // Sends Current state of Toggle Button to
+    // Pixy2
 
-    ColorSensor.setup();
-
-    m_PDP = new PowerDistributionPanel(0);
-
-    kMotor1 = new Talon(kMotor1Port);
-    kMotor1.setInverted(false);
-
-    kMotor2 = new Talon(kMotor2Port);
-    kMotor2.setInverted(false);
-
-    sUltrasonic1 = new AnalogInput(sUltrasonic1Port);
-
-    /*
-     * sUltrasonic1.setOversampleBits(4); bits = sUltrasonic1.getOversampleBits();
-     * sUltrasonic1.setAverageBits(2); bits = sUltrasonic1.getAverageBits();
-     */
-
-    raw = sUltrasonic1.getValue();
-    volts = sUltrasonic1.getVoltage();
-    averageRaw = sUltrasonic1.getAverageValue();
-    averageVolts = sUltrasonic1.getAverageVoltage();
+    colorsensor = new ColorSensor(I2C.Port.kOnboard);
 
     xbox = new XboxController(0);
     joystick = new Joystick(1);
@@ -84,10 +44,15 @@ public class Robot extends TimedRobot {
   @Override
   public void robotPeriodic() {
     Scheduler.getInstance().run();
-    Cameras.run(); // Runs Pixy2 and Microsoft Camera
-
-    boolean PixyLightState = SmartDashboard.getBoolean("Pixy2 Light", false);
-    Cameras.light(PixyLightState); // Sends Current state of Toggle Button to Pixy2
+    // Cameras.run(); // Runs Pixy2 and Microsoft Camera
+    colorsensor.read();
+    System.out.println(colorsensor.getRed());
+    System.out.println(colorsensor.getGreen());
+    System.out.println(colorsensor.getBlue());
+    System.out.println(colorsensor.getProx());
+    // boolean PixyLightState = SmartDashboard.getBoolean("Pixy2 Light", false);
+    // Cameras.light(PixyLightState); // Sends Current state of Toggle Button to
+    // Pixy2
 
   }
 
@@ -113,14 +78,18 @@ public class Robot extends TimedRobot {
 
   @Override
   public void teleopInit() {
-
+    colorsensor = new ColorSensor(I2C.Port.kOnboard);
   }
 
   @Override
   public void teleopPeriodic() {
     Scheduler.getInstance().run();
-    // analogValue = getVoltage(sUltrasonic1);
-    System.out.println(analogValue);
+    colorsensor.read();
+    System.out.println(colorsensor.getRed());
+    System.out.println(colorsensor.getGreen());
+    System.out.println(colorsensor.getBlue());
+    System.out.println(colorsensor.getProx());
+
   }
 
   public int getDriveStationNumber() {
--- src/main/java/frc/team4546/robot/subsystems/ColorSensor.java
+++ src/main/java/frc/team4546/robot/subsystems/ColorSensor.java
@@ -1,15 +1,12 @@
 package frc.team4546.robot.subsystems;
 
-import edu.wpi.first.wpilibj.*;
-
-import frc.team4546.robot.Dashboard;
+import edu.wpi.first.wpilibj.I2C;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 
 public class ColorSensor {
-	private static I2C sensor;
-
-	protected final static int COMMAND_REGISTER_BIT = 0x80;
+	protected final static int CMD = 0x80;
 	protected final static int MULTI_BYTE_BIT = 0x20;
-
 	protected final static int ENABLE_REGISTER = 0x00;
 	protected final static int ATIME_REGISTER = 0x01;
 	protected final static int PPULSE_REGISTER = 0x0E;
@@ -21,30 +18,78 @@ public class ColorSensor {
 	protected final static int BDATA_REGISTER = 0x1A;
 	protected final static int PDATA_REGISTER = 0x1C;
 
-	public static void setup() {
-		sensor = new I2C(I2C.Port.kOnboard, 0x39); // port, I2c address
+	protected final static int PON = 0b00000001;
+	protected final static int AEN = 0b00000010;
+	protected final static int PEN = 0b00000100;
+	protected final static int WEN = 0b00001000;
+	protected final static int AIEN = 0b00010000;
+	protected final static int PIEN = 0b00100000;
 
-		sensor.write(COMMAND_REGISTER_BIT | 0x00, 0b00000011); // power on, color sensor on
+	private final double integrationTime = 10;
+
+	private I2C sensor;
+
+	private ByteBuffer buffy = ByteBuffer.allocate(8);
+
+	public int red = 0, green = 0, blue = 0, prox = 0;
+
+	public ColorSensor(I2C.Port port) {
+		buffy.order(ByteOrder.LITTLE_ENDIAN);
+		sensor = new I2C(port, 0x39); // 0x39 is the address of the Vex ColorSensor V2
+
+		sensor.write(CMD | 0x00, PON | AEN | PEN);
+
+		sensor.write(CMD | 0x01, (int) (256 - integrationTime / 2.38)); // configures the integration time (time for
+																		// updating color data)
+		sensor.write(CMD | 0x0E, 0b1111);
+
 	}
 
-	public static void red() {
-		Dashboard.getInstance().putNumber(false, "Red Value", RDATA_REGISTER);
+	public void read() {
+		buffy.clear();
+		sensor.read(CMD | MULTI_BYTE_BIT | RDATA_REGISTER, 8, buffy);
+
+		red = buffy.getShort(0);
+		if (red < 0) {
+			red += 0b10000000000000000;
+		}
+
+		green = buffy.getShort(2);
+		if (green < 0) {
+			green += 0b10000000000000000;
+		}
+
+		blue = buffy.getShort(4);
+		if (blue < 0) {
+			blue += 0b10000000000000000;
+		}
+
+		prox = buffy.getShort(6);
+		if (prox < 0) {
+			prox += 0b10000000000000000;
+		}
+
 	}
 
-	public static void green() {
-		Dashboard.getInstance().putNumber(false, "Green Value", GDATA_REGISTER);
+	public int status() {
+		buffy.clear();
+		sensor.read(CMD | 0x13, 1, buffy);
+		return buffy.get(0);
 	}
 
-	public static void blue() {
-		Dashboard.getInstance().putNumber(false, "Blue Value", BDATA_REGISTER);
+
+	public int getRed(){
+		return red;
+	}
+	public int getGreen(){
+		return green;
+	}
+	public int getBlue(){
+		return blue;
+	}
+	public int getProx(){
+		return prox;
 	}
 
-	public static void clear() {
-		Dashboard.getInstance().putNumber(false, "Clear Value", CDATA_REGISTER);
-	}
-
-	public static void proximity() {
-		Dashboard.getInstance().putNumber(false, "Proximity Value", PDATA_REGISTER);
-	}
 
 }
