e0b65dee51ee350fa28b2cea7615178f982d81ad
Sage Coff <pixelmoongaming4702@gmail.com>
2019-02-09 14:39:10 -0500

    added oi file

---
+++ src/main/java/frc/team4546/robot/subsystems/OI.java
@@ -0,0 +1,11 @@
+package frc.team4546.robot.subsystems;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+public class OI{
+
+    
+    public OI(){
+
+    }
+}
