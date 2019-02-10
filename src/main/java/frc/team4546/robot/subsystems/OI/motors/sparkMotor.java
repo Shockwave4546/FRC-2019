e08d2f5056279287d837425c8eec343162af0fff
Sage Coff <pixelmoongaming4702@gmail.com>
2019-02-10 14:00:22 -0500

    updated spark class to work with spark max controllers that we are using for brushless motor

--- src/main/java/frc/team4546/robot/subsystems/OI/motors/sparkMotor.java
+++ src/main/java/frc/team4546/robot/subsystems/OI/motors/sparkMotor.java
@@ -1,12 +1,18 @@
 package frc.team4546.robot.subsystems.OI.motors;
 import frc.team4546.robot.subsystems.OI.motors.Motor;
-import edu.wpi.first.wpilibj.Spark;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+
+import edu.wpi.first.wpilibj.SpeedController;
 
 public class sparkMotor extends Motor{
-    private Spark kMotor;
+    private CANSparkMax kMotor;
+    //public static final CANSparkMaxLowLevel.MotorType kBrushless;
     public sparkMotor(final int port, final double pos, final double neg){
         super(port, pos, neg);
-        kMotor = new Spark(port);
+        kMotor = new CANSparkMax(port, MotorType.kBrushless);
     }
     public void rotateClockwise(final double rotate){
         kMotor.set(rotate * mPos);
