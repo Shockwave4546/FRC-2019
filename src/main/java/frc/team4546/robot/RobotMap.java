d410f11124c516eef14eddceeee5f36d656e4cba
Gabe-B <gabeblack3@gmail.com>
2019-02-15 19:47:51 -0500

    Revert "Merge branch 'driveBase2.0' of https://github.com/Shockwave4546/FRC-2019 into driveBase2.0"
    This reverts commit 31ed83a4fdf54a7108477e65ae3de6109dd59220, reversing
    changes made to bcda795f59f1561d5bff6be49d71110a98b78395.

---
+++ src/main/java/frc/team4546/robot/RobotMap.java
@@ -0,0 +1,55 @@
+package frc.team4546.robot;
+
+/**
+ * The RobotMap is a mapping from the ports sensors and actuators are wired into
+ * to a variable name. This provides flexibility changing wiring, makes checking
+ * the wiring easier and significantly reduces the number of magic numbers
+ * floating around.
+ */
+public class RobotMap {
+    // Joystick Ports //
+        public static final int XboxDriver = 0;
+        public static final int XboxCoDriver = 1;
+
+
+    // PWM Channels //
+        // public static final int DRIVE_TALONSR_LEFT_CHANNEL = 0;
+        public static final int LeftDrivePort = 0;
+        public static final int RightDrivePort = 1;
+        public static final int LeftIntakePort = 2;
+        public static final int RightIntakePort = 3;
+        public static final int LinearSlidePort = 4;
+        public static final int IntakePivotPort = 5;
+
+
+    // CAN IDs //
+        // public static final int PCMone = 0;
+        // public static final int PCMtwo = 1;
+
+    // Digital Channels //
+        // public static final int DRIVE_LEFT_ENCODER = 1;
+        // public static final int DRIVE_RIGHT_ENCODER = 2;
+
+    // Solenoid Channels //
+
+    // Drive Train Motor Speeds (POS & NEG) //
+        public static final double LeftDrivePos = .2;
+        public static final double LeftDriveNeg = .2;
+        public static final double RightDrivePos = .2;
+        public static final double RightDriveNeg = .2;
+
+    // Misc Motor Speeds //
+
+    // Lift Positions //
+         public static final int GroundLevelBall = 1420; 
+
+        public static final int LowHatchPanelHeight = 1420; // Same as Ground Level for Alliance Station and Cargo Ship and First Level Rocket.
+        public static final int MidHatchPanelHeight = 1420;
+        public static final int HighHatchPanelHeight = 1420;
+
+        public static final int CargoShipBallHeight = 1420; // Cargo Ship Ball Height
+        public static final int RocketLowBallHeight = 1420; // Rocket Ball Low
+        public static final int RocketMidBallHeight = 1420; // Rocket Ball Mid
+        public static final int RocketHighBallHeight = 1420; // Rocket Ball High
+
+}
