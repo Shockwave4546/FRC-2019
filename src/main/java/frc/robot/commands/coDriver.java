cdb274393952a39835732e3e33911c19f064979d
Sage Coff <pixelmoongaming4702@gmail.com>
2019-02-18 15:05:59 -0500

    added hatch outtake pneumatics control

--- src/main/java/frc/robot/commands/coDriver.java
+++ src/main/java/frc/robot/commands/coDriver.java
@@ -1,6 +1,7 @@
 package frc.robot.commands;
 
 import frc.robot.subsystems.motors.*;
+import frc.robot.subsystems.shockwaveSolenoid;
 import frc.robot.controllers.shockwaveXbox;
 import frc.robot.RobotMap;
 
@@ -9,17 +10,20 @@ public class coDriver{
     private victorMotor kIntakeRight;
     private victorMotor kLinearSlide;
     private shockwaveXbox cCoDriverXbox;
+    private shockwaveSolenoid pHatchIntake;
     private boolean cIntakeLeft;
     private boolean cIntakeRight;
     private double cLinearSlide;
+    private boolean cDriveAButton;
     public coDriver(){
         kIntakeLeft = new victorMotor(RobotMap.LeftIntakePort, RobotMap.LeftIntakePos, RobotMap.LeftIntakeNeg);
         kIntakeRight = new victorMotor(RobotMap.RightIntakePort, RobotMap.RightIntakePos, RobotMap.RightIntakeNeg);
         kLinearSlide = new victorMotor(RobotMap.LinearSlidePort, RobotMap.LinearSlidePos, RobotMap.LinearSlideNeg);
+        pHatchIntake = new shockwaveSolenoid(RobotMap.pIntakeHatchF, RobotMap.pIntakeHatchR);
         cCoDriverXbox = new shockwaveXbox(RobotMap.XboxCoDriver);
     }
 
-    private void intakeControl(){
+    private void intakeBallControl(){
         cIntakeLeft = cCoDriverXbox.getLeftBumper();
         cIntakeRight = cCoDriverXbox.getRightBumper();
         if(cIntakeLeft == true){
@@ -44,9 +48,19 @@ public class coDriver{
         kLinearSlide.rotateMotor(cLinearSlide);
     }
 
+    public void intakeHatchControl(){
+        cDriveAButton = cCoDriverXbox.getAbutton();
+        if(cDriveAButton == true){
+            pHatchIntake.forward();
+        }else{
+            pHatchIntake.reverse();
+        }
+    }
+
     public void coDrive(){
-        intakeControl();
-        //slideControl();
+        intakeBallControl();
+        intakeHatchControl();
+        slideControl();
     }
     public void remoteIntake(final double speed){
         kIntakeLeft.rotateMotor(speed);
