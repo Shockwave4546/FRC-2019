eefbe004efa5a020e7de26d97f8cbd3e0d31a29c
Dilan Patel <dilanpace1496@gmail.com>
2019-02-18 19:33:10 -0500

    Revert "e"
    This reverts commit 056534ac9280328bcaac4d5a3d6c63d810613506.

--- src/main/java/frc/robot/commands/Driver.java
+++ src/main/java/frc/robot/commands/Driver.java
@@ -3,7 +3,10 @@ package frc.robot.commands;
 import frc.robot.subsystems.motors.*;
 import frc.robot.subsystems.shockwaveSolenoid;
 import frc.robot.controllers.shockwaveXbox;
+import edu.wpi.first.wpilibj.XboxController;
 import frc.robot.RobotMap;
+import frc.robot.subsystems.sensors.ColorSensor;
+import frc.robot.Dashboard;
 
 public class Driver{
     private sparkMotor kLeftDrive;
@@ -15,10 +18,7 @@ public class Driver{
     private boolean intakeToggle;
     private boolean cDriveAButton;
     private boolean cDriveBButton;
-<<<<<<< HEAD
     public static shockwaveXbox xController;
-=======
->>>>>>> parent of 91270c8... Added line detection
     public Driver(){
         kLeftDrive = new sparkMotor(RobotMap.LeftDrivePort,RobotMap.LeftDrivePos,RobotMap.LeftDriveNeg);
         kRightDrive = new sparkMotor(RobotMap.RightDrivePort,RobotMap.RightDrivePos,RobotMap.RightDriveNeg);
