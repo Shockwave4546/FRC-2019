cb79758f003bc37e1af6b629da652021253a70b7
Sage Coff <pixelmoongaming4702@gmail.com>
2019-09-19 19:36:50 -0400

    idk what happened here tbh

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -8,7 +8,6 @@ import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-
 import frc.robot.subsystems.shockwaveSolenoid;
 
 public class Robot extends TimedRobot {
