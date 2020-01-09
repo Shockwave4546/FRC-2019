cb93c94e6b4a5ea46111731fb7faae0b056606c6
Sage Coff <pixelmoongaming4702@gmail.com>
2020-01-09 18:50:09 -0500

    eh

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -5,9 +5,9 @@ public class RobotMap {
         public static final int XboxDriver = 0;
 
     // Motor IDs //
-        public static final int LeftDrivePort = 0;      // SparkMax Motor 1     -PDP: 12
-        public static final int RightDrivePort = 1;     // SparkMax Motor 2     -PDP: 14
-        public static final int MiddleDrivePort = 2;
+        public static final int LeftDrivePort = 0; // Talon
+        public static final int RightDrivePort = 1; // Talon
+        public static final int MiddleDrivePort = 2; // Spark
 
     // Drive Train Motor Speeds (POS & NEG) //
         public static final double LeftDrivePos = .5;
